6482c3521dec08f01b438dfb06e03424c5c8b8e1
raz0005 <161780046+raz0005@users.noreply.github.com>
2024-06-19 19:47:56 -0400

    Add files via upload

---
+++ Project 3/QuickDBD-export (1).sql
@@ -0,0 +1,31 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Products" (
+    "ProductID" INT   NOT NULL,
+    "ProductName" varchar(50)   NOT NULL,
+    "Rating" INT   NOT NULL,
+    "NumReviews" INT   NOT NULL,
+    "DateAdded" DATE   NOT NULL,
+    CONSTRAINT "pk_Products" PRIMARY KEY (
+        "ProductID"
+     )
+);
+
+CREATE TABLE "Transactions" (
+    "TransactionID" INT   NOT NULL,
+    "ProductID" INT   NOT NULL,
+    "Price" DECIMAL   NOT NULL,
+    "Discount" DECIMAL   NOT NULL,
+    "Sales" INT   NOT NULL,
+    "StockQuantity" INT   NOT NULL,
+    "Category" TEXT   NOT NULL,
+    CONSTRAINT "pk_Transactions" PRIMARY KEY (
+        "TransactionID"
+     )
+);
+
+ALTER TABLE "Transactions" ADD CONSTRAINT "fk_Transactions_ProductID" FOREIGN KEY("ProductID")
+REFERENCES "Products" ("ProductID");
+
